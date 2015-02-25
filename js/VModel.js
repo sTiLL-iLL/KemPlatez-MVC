b7d57d1aaf8a065dc1774c14486a2e386973e605
Heath Dernovich <sTiLL-iLL@users.noreply.github.com>
2015-02-24 23:24:29 -0700

    Rename ViewModel.js to VModel.js

---
+++ js/VModel.js
@@ -0,0 +1,18 @@
+
+// model
+
+var user = {
+	name1: "Richard",
+	name2: "Longwood",
+	occupation: "Software Engineer",
+	age: 40,
+	title: 'User!',
+	subTitle: "Edit Me!",
+	canEdit: true,
+	skills: ["ajax", "css", "javascript", "HTML5", ".Net", "SQL", "json"]
+};
+
+// create an observable model of a user...
+
+SeeMe.base = user;
+var User = Object.create(SeeMe);
